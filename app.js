1df61cad74bb73b596d1bb2c564aec97657e55d8
Tina2945 <103306006@nccu.edu.tw>
2019-05-16 01:02:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-var linebot = require('linebot');
-
-// 用於辨識Line Channel的資訊
-var bot = linebot({
-  channelId: '1575013516',
-  channelSecret: '6aca82a49ca471da2a0754c3a7e33072',
-  channelAccessToken: 'vKWmj1jI9ypiYzibwWIpirFlWAgBu7jze0xeoE8pLH13PsiyntmDfzFXgYqfjUT+5w8tOAkkdMkIiVpuRFY0eo+tUrx2XYkByQ5a5yiSeO1XftTkMZGdRY5mOcjsfe1TgFLuNlZHGhoBHmRoqFIfJgdB04t89/1O/w1cDnyilFU='
+const line = require('@line/bot-sdk');
+const express = require('express');
+const lineConfig = {
+  channelAccessToken: process.env.KWmj1jI9ypiYzibwWIpirFlWAgBu7jze0xeoE8pLH13PsiyntmDfzFXgYqfjUT+5w8tOAkkdMkIiVpuRFY0eo+tUrx2XYkByQ5a5yiSeO1XftTkMZGdRY5mOcjsfe1TgFLuNlZHGhoBHmRoqFIfJgdB04t89/1O/w1cDnyilFU=,
+  channelSecret: process.env.6aca82a49ca471da2a0754c3a7e33072
+};
+const client = new line.Client(lineConfig);
+const app = express();
+app.listen(3000, function() {
+  console.log('App now running on port', this.address().port);
 });
-
-// 當有人傳送訊息給Bot時
-bot.on('message', function (event) {
-  // event.message.text是使用者傳給bot的訊息
-  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
-  var replyMsg = `Hello你剛才說的是:${event.message.text}`;
-  event.reply(event.message.text).then(function (data) {
-    // 當訊息成功回傳後的處理
-  }).catch(function (error) {
-    // 當訊息回傳失敗後的處理
-  });
+app.post('/', line.middleware(lineConfig), function(req, res) {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then(function(result) {
+      res.json(result);
+    });
 });
-
-// Bot所監聽的webhook路徑與port
-bot.listen('/linewebhook', 3000, function () {
-    console.log('[BOT已準備就緒]');
-});
+function handleEvent(event) {
+  switch (event.type) {
+    case 'join':
+    case 'follow':
+      return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: '你好請問我們認識嗎?'
+      });   
+    case 'message':
+      switch (event.message.type) {
+        case 'text':
+          return client.replyMessage(event.replyToken, {
+            type: 'text',
+            text: (event.message.text+'~*')
+          });
+      }
+  }
+}
